d2e6c83c4d74fa27dc0dc8682616b7084a44a09c
DESKTOP-G8GH63Q\ApproX <diego.matias@approx.com.br>
2016-11-01 14:01:48 -0200

    first commit

---
+++ Assets/scripts/AirPlane.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(MyFisica))]
+public class AirPlane : MonoBehaviour {
+	MyFisica fisica;
+	float forceAr;// força do ar com base na altitude
+	float altitude;
+	public float MotorForce; // força maxima que o motor alcança
+	float motorFactor; // força atula do motor varia de 0 a 1;
+
+
+	// Use this for initialization
+	void Awake () {
+		fisica = this.GetComponent<MyFisica> ();
+	}
+
+	void setArForce(){ // seta a força do ar com base na altitude
+		forceAr=1-(altitude/1500);
+	}
+
+	void CalculaAltitude() 
+	{
+		altitude = this.transform.position.y*10;
+	}
+
+
+
+	void setForces()// calcula as forças exercidar no avião e seta elas na fisica
+	{
+		CalculaAltitude ();
+		setArForce ();
+		Vector3 force = Vector3.forward;
+		force *= (MotorForce * motorFactor);// calcula a força com base no motor e o qual aquele motor está sendo usado
+		fisica.AddForce(force);
+		Vector3 arFoce = Vector3.up;
+		arFoce *= ((MotorForce * motorFactor) * forceAr)/fisica.massa;
+		fisica.AddForce (arFoce);
+	}
+
+	void Acelera()
+	{
+		if(motorFactor<1f)
+		{
+			motorFactor += 0.3f * Time.deltaTime;
+		}
+		setForces ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Acelera ();
+		}
+	
+	}
+}
---
+++ Assets/scripts/MyFisica.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyFisica : MonoBehaviour {
+	public float massa;
+	public float gravidade;
+	public float ResistenciaAr;
+	public float angularDrag;
+
+	Collider collider;
+	List<Vector3> forces;
+	Vector3 gravityForce;
+	Vector3 aceleration;
+	Vector3 angularAceleration;
+	Vector3 arForce;
+	Vector3 inercia;
+	Vector3 cineticEnergy;
+
+	Vector3 initialPose;
+	Vector3 finalPose;
+
+	float velociade=0;
+	Vector3 peso;
+
+	bool onGround=false;
+
+	// Use this for initialization
+	void Start () {
+		forces = new List<Vector3> ();
+		gravityForce = Vector3.zero;
+		gravityForce.y = -gravidade;
+		peso = gravityForce * massa;
+	}
+
+	void CalculaInercia()// calcula inercia com base no tipo de colider
+	{
+		BoxCollider box = GetComponent<BoxCollider> ();
+		if (box == null) {
+			SphereCollider sphere = GetComponent<SphereCollider> ();
+			float i = (2.0f / 3f) * massa * (sphere.radius * sphere.radius);
+			inercia = new Vector3 (i,i,i);
+			return;
+		}
+		inercia = new Vector3 ((1/12)*massa*(Mathf.Pow(box.size.y,2)+Mathf.Pow(box.size.z,2)),(1/12)*massa*(Mathf.Pow(box.size.x,2)+Mathf.Pow(box.size.z,2)),(1/12)*massa*(Mathf.Pow(box.size.x,2)+Mathf.Pow(box.size.y,2)));
+	}
+
+	public void OnCollisionStay(Collision col)
+	{
+		onGround = true;
+	}
+
+	public void OnCollisionExit(Collision col)
+	{
+		onGround = false;
+	}
+
+	Vector3 SomatorioForces(){ // retorna o somatorio das forças exercidas nele
+		if (!onGround) {// se estiver no chão não calcula gravidade
+			forces.Add (peso);
+		}
+		Vector3 resultante=Vector3.zero;
+		for (int i = 0; i < forces.Count; i++) {
+			resultante += forces [i];
+		}
+		forces.Clear ();
+		return (resultante) / massa;
+	}
+
+	public void AddForce(Vector3 f) // adiciona uma força 
+	{
+		forces.Add (f);
+	}
+
+	void AngularAceleration(Vector3 torque)// calcula a aceleração angular com base no torque
+	{
+		angularAceleration = new Vector3(torque.x / inercia.x, torque.y / inercia.y, torque.z / inercia.z);
+		angularAceleration -= angularAceleration * angularDrag * Time.deltaTime;
+	}
+
+	Vector3 ElevaVetor(Vector3 vetor, float indice)// metodo para elevar um vetor a um numero
+	{
+		return new Vector3 (Mathf.Pow (vetor.x, indice), Mathf.Pow (vetor.y, indice), Mathf.Pow (vetor.z, indice));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		velociade = aceleration.magnitude;
+
+		if (velociade == 0) {
+			initialPose = this.transform.position;
+		}
+
+		cineticEnergy = 0.5f * massa * ElevaVetor(aceleration,2);
+		aceleration =SomatorioForces ()*Time.deltaTime;
+
+		this.transform.position += aceleration;
+		transform.rotation *= Quaternion.Euler (angularAceleration);
+	}
+}
