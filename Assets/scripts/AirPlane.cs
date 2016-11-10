962d2eaeb780cffaec69f71a07439302401f395f
ImDiegoDM <dm.diego.bh@gmail.com>
2016-11-10 02:03:41 -0200

    Ajustado Valor da Fisica e adicionado Animações

--- Assets/scripts/AirPlane.cs
+++ Assets/scripts/AirPlane.cs
@@ -20,6 +20,16 @@ public class AirPlane : MonoBehaviour {
 	public float lemeforce;
     public float ArConstant;
 
+	//---- variaveis de animação------
+	float aileronL=0;
+	float aileronR=0;
+	float profundorUp=0;
+	float profundorDown=0;
+	float lemeR=0;
+	float lemeL=0;
+	int helice=0;
+	Animator anim;
+
 
 
     Vector3 oldPostion=Vector3.zero;
@@ -29,14 +39,20 @@ public class AirPlane : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
+		anim = this.GetComponentInChildren<Animator> ();
 		fisica = this.GetComponent<MyFisica> ();
 		superficeFront = areaDeSuperficie;
 	}
 
 	void setArForce(){ // seta a força do ar com base na altitude
-		forceAr=1f-(altitude/2500f);
-        if (forceAr < 0.1f) {
-			forceAr = 0.1f;
+		try{
+			forceAr=(35f/altitude);
+		}
+		catch {
+			forceAr=(35f/altitude+0.01f);
+		}
+        if (forceAr < 0.01f) {
+			forceAr = 0.01f;
 		}
 	}
 
@@ -47,7 +63,7 @@ public class AirPlane : MonoBehaviour {
 
 	void CalculaAltitude() 
 	{
-		altitude = this.transform.position.y*10f;
+		altitude = this.transform.position.y;
 	}
 
     Vector3 CalculaVelocidade()
@@ -66,14 +82,22 @@ public class AirPlane : MonoBehaviour {
 
 		Vector3 force = transform.forward;
         force *= (MotorForce * motorFactor);// calcula a força com base no motor e o qual aquele motor está sendo usado
+		force.y=0;
 
 		float superficieAtual=areaDeSuperficie;// seta a area de superfice do avião
+		Vector3 arFoce;
+		if (this.transform.eulerAngles.x < 10 || this.transform.eulerAngles.x > 180) {
+			arFoce = transform.up;
+		} else {
+			arFoce = transform.up*-1;
+		}
 
-		Vector3 arFoce = transform.up;
-        arFoce *= fisica.getVelocity().magnitude*ArConstant*forceAr;// calcula a força do ar com base na velocidade do avião
+
+		arFoce *= ArConstant*(forceAr)*superficieAtual*Mathf.Pow ((fisica.getVelocity().magnitude),2)/2;// calcula a força do ar com base na velocidade do avião
 		//se estiver muito inclinado a potencia do motor cai para 40% e a superfice de contato do avião aumenta drasticamente
+		print(this.transform.eulerAngles.x);
 		if ((this.transform.eulerAngles.x < 330f && this.transform.eulerAngles.x > 210f) || (this.transform.eulerAngles.x < 150f && this.transform.eulerAngles.x > 30f)) {
-            force = 40f * force/ 100f;
+            //force = 70f * force/ 100f;
 			superficieAtual *= 5f;
             print("muito inclinado");
 		}
@@ -81,6 +105,7 @@ public class AirPlane : MonoBehaviour {
 		// se o angulo de atack for muito inclinado o força do motor cai para 5%
 		if ((this.transform.eulerAngles.x < 290f && this.transform.eulerAngles.x > 250f) || (this.transform.eulerAngles.x < 100f && this.transform.eulerAngles.x > 70f)) {
             force = 5f * force/ 100f;
+			print("muito inclinado mesmo");
 		}
 
 		//ponto onde ira acontecer uma leve inclinação se o aviao não estiver em equilibrio
@@ -93,7 +118,7 @@ public class AirPlane : MonoBehaviour {
 		}
 
 		//calcula a força do ar que colide com a frente do avião
-        float drag = coeficienteAeroDinamica * (forceAr / 2) * superficieAtual * Mathf.Pow ((fisica.getVelocity().magnitude*2),2);
+		float drag = coeficienteAeroDinamica * (forceAr / 2f) * superficieAtual * Mathf.Pow ((fisica.getVelocity().magnitude),2);
 
         fisica.AddForce(force);
         fisica.AddForce (arFoce);
@@ -106,6 +131,7 @@ public class AirPlane : MonoBehaviour {
 	/// </summary>
 	public void AileronsLeft(){
 		fisica.ApplyTorqueByPointForce (aileronsRightPosition.position,transform.up*aileronsForce);
+		StartAnim (ref aileronL,ref aileronR,"AileronL","AileronR",0.05f);
 	}
 
 
@@ -114,6 +140,7 @@ public class AirPlane : MonoBehaviour {
 	/// </summary>
 	public void AileronsRight(){
 		fisica.ApplyTorqueByPointForce (aileronsLeftPosition.position,transform.up*aileronsForce);
+		StartAnim (ref aileronR,ref aileronL,"AileronR","AileronL",0.05f);
 	}
 
 	/// <summary>
@@ -121,6 +148,7 @@ public class AirPlane : MonoBehaviour {
 	/// </summary>
 	public void ProfundoresUP(){
 		fisica.ApplyTorqueByPointForce (profundoresPosition.position,(transform.up*-1)*profundoresForce);
+		StartAnim (ref profundorUp,ref profundorDown,"ProfundorUp","ProfundorD",0.05f);
 	}
 
 
@@ -131,6 +159,7 @@ public class AirPlane : MonoBehaviour {
 		Vector3 point = this.transform.position;
 		point.z -= 1.5f;
 		fisica.ApplyTorqueByPointForce (profundoresPosition.position,transform.up*profundoresForce);
+		StartAnim (ref profundorDown,ref profundorUp,"ProfundorD","ProfundorUp",0.05f);
 	}
 
 	/// <summary>
@@ -138,6 +167,7 @@ public class AirPlane : MonoBehaviour {
 	/// </summary>
 	public void LemeLeft(){
 		fisica.ApplyTorqueByPointForce (lemePosition.position,transform.right*-1*lemeforce);
+		StartAnim (ref lemeL,ref lemeR,"LemeL","LemeR",0.05f);
 	}
 
 	/// <summary>
@@ -147,6 +177,21 @@ public class AirPlane : MonoBehaviour {
 		Vector3 point = this.transform.position;
 		point.z += 1.5f;
 		fisica.ApplyTorqueByPointForce (lemePosition.position,transform.right*lemeforce);
+		StartAnim (ref lemeR,ref lemeL,"LemeR","LemeL",0.05f);
+	}
+
+	void StartAnim(ref float variavelAdd,ref float variavelSub,string nomeParAdd,string nomeParSub,float fator){
+		variavelAdd += fator;
+		if (variavelAdd >= 1f) {
+			variavelAdd = 1f;
+		}
+
+		variavelSub -= fator;
+		if (variavelSub <= 0f) {
+			variavelSub = 0f;
+		}
+		anim.SetFloat (nomeParAdd,variavelAdd);
+		anim.SetFloat (nomeParSub,variavelSub);
 	}
 
 	/// <summary>
@@ -155,14 +200,23 @@ public class AirPlane : MonoBehaviour {
 	/// <param name="fator">Fator do motor que ira usar</param>
 	public void forçaMotor(float fator)
 	{
+		anim.SetFloat ("heliceSpeed",fator);
 		if(motorFactor<fator)
 		{
-			motorFactor += 0.1f * Time.deltaTime;
+			motorFactor += 0.05f * Time.deltaTime;
 		}
 		else
 		{
 			motorFactor = fator;
 		}
+	}
+
+	void parametroBackNormal(ref float parametro,float fator,string nameAnimPar){
+		parametro -= fator;
+		if (parametro <= 0f) {
+			parametro = 0f;
+		}
+		anim.SetFloat (nameAnimPar, parametro);
 	}
 
 	public float getAltitude()
@@ -182,19 +236,28 @@ public class AirPlane : MonoBehaviour {
 			ProfundoresUP ();
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			ProfundoresDown ();
+		} else {
+			parametroBackNormal (ref profundorUp,0.05f,"ProfundorUp");
+			parametroBackNormal (ref profundorDown,0.05f,"ProfundorD");
 		}
 
 		if (Input.GetKey (KeyCode.A)) {
 			LemeLeft ();
 		} else if (Input.GetKey (KeyCode.D)) {
 			LemeRigth ();
+		} else {
+			parametroBackNormal (ref lemeL,0.05f,"LemeL");
+			parametroBackNormal (ref lemeR,0.05f,"LemeR");
 		}
+			
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			AileronsLeft ();
-		}
-		else if (Input.GetKey (KeyCode.RightArrow)) {
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			AileronsRight ();
+		} else {
+			parametroBackNormal (ref aileronL,0.05f,"AileronL");
+			parametroBackNormal (ref aileronR,0.05f,"AileronR");
 		}
 		setForces ();
 	
